4603eb3b48e383899a5841b639c4c0d08e87e15c
shiv0527 <68030497+shiv0527@users.noreply.github.com>
2022-02-03 18:17:16 -0500

    Add files via upload

---
+++ Two_Sums.java
@@ -0,0 +1,35 @@
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Two_Sums {
+
+	
+	
+	    public static int[] twoSum(int[] nums, int target) {
+	        int[] result = new int[2];
+	        Map<Integer,Integer> store = new HashMap<Integer,Integer>();
+	        for(int i =0; i<nums.length; i++)
+	        {
+	            if(store.containsKey(target - nums[i]))
+	            {
+	                result[1] = i;
+	                result[0] = store.get(target - nums[i]);
+	                //return result;
+	                System.out.println(""+ Arrays.toString(result));
+	            }
+	            store.put(nums[i], i);
+	        }
+	        return result;
+	    }
+	
+	    
+	    public static void main(String[] args)
+	    {
+	    	int num[] = {1,2,3,4};
+	    	int target = 7;
+	    	twoSum(num, target);
+	    }
+	
+}
